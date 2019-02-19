da455e05ffb2787ccd465d765e07aec9b31ced9b
DmitryVakhrushev <dm.vakhrushev@gmail.com>
2019-02-18 22:50:14 -0500

    Starting Bucky's tutorial on C,

---
+++ C_BuckyRoberts/Bucky04_PrintTextToScreen/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Hello world!\n");/**< new line */
+    printf("Hello Kitty\t"); /**< tab */
+    printf("Hi Dmitry\a");   /**< alert sound*/
+    return 0;
+}
---
+++ C_BuckyRoberts/Bucky05_Comments/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    // This is a single line comment
+    printf("Hello Dmitry!\n");
+
+    /*
+    This is
+    a multiline
+    comment
+    */
+
+    return 0;
+}
