cb741d018faf4a6c19d36fcbb29ecea842f721e0
DmitryVakhrushev <dm.vakhrushev@gmail.com>
2019-02-24 22:08:29 -0500

    Added lessons 6 and 7.

---
+++ C_BuckyRoberts/ Bucky06_ConversionCharacters/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("%s is the best %s ever. \n", "Bucky", "programmer"); // string
+
+    printf("I ate %d corndogs last night \n", 9); // integer
+
+    printf("Pi is %f \n", 3.14535789); // float
+    printf("Pi is %.2f \n", 3.14535789); // show 2 decimals
+    printf("Pi is %.4f \n", 3.14535789); // show 4 decimals
+
+    return 0;
+}
---
+++ C_BuckyRoberts/Bucky07_Variables/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    int age;
+    age = 27;
+    printf("Bucky is %d years old\n", age);
+    return 0;
+}
