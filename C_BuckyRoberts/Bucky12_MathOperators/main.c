db8922070c1202909704491c8edb818033b887d1
DmitryVakhrushev <dm.vakhrushev@gmail.com>
2019-03-06 16:35:06 -0500

    Lessons 11 and 12

---
+++ C_BuckyRoberts/Bucky12_MathOperators/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int weight = 595;
+
+    // Operators: *, /, +, -, %
+    printf("If I eat a watermelon I will weigh %d lbs \n", weight + 12);
+
+    // INTEGRES
+    int a = 86;
+    int b = 21;
+    printf("%d \n", a/b);
+
+    // FLOATS
+    float c = 86.0;
+    float d = 21.0;
+    printf("%f \n", c/d);
+
+    return 0;
+}
