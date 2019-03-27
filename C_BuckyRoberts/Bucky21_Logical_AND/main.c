ba0d18c5983c6d274bab3e0b7e120c5e19526cce
DmitryVakhrushev <dm.vakhrushev@gmail.com>
2019-03-27 15:18:25 -0400

    (1) if, (2) if-else, (3) if-elseif-else

---
+++ C_BuckyRoberts/Bucky21_Logical_AND/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	int hoursStudied = 60; //Required 10 or more
+	int kidsBeatUp = 0;    //Required 0 kids
+
+	if( (hoursStudied >=10) && (kidsBeatUp==0) ){
+
+		printf("You are a good student");
+	}
+
+    return 0;
+}
