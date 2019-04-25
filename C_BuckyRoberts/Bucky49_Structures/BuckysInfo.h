60cac3e60dffa68255f63ba2f66764c75c5a0dba
DmitryVakhrushev <dm.vakhrushev@gmail.com>
2019-04-25 10:45:37 -0400

    Structures

---
+++ C_BuckyRoberts/Bucky49_Structures/BuckysInfo.h
@@ -0,0 +1,11 @@
+
+struct user{
+
+    // Members of the structure
+    int userID;
+    char firstName[25];
+    char lastName[25];
+    int age;
+    float weight;
+
+};
---
+++ C_BuckyRoberts/Bucky49_Structures/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "BuckysInfo.h"
+
+int main()
+{
+
+    // Structures - group several variables of DIFFERENT types
+    struct user bucky;
+    struct user emily;
+
+    bucky.userID = 1;
+    emily.userID = 2;
+
+    puts("Enter the first name of user 1");
+    gets(bucky.firstName);
+
+    puts("Enter the first name of user 2");
+    gets(emily.firstName);
+
+    printf("User 1 id is  %d\n", bucky.userID);
+    printf("User 2 first name is  %s\n", emily.firstName);
+
+    return 0;
+}
