37fd214d04cd264c24e11c9203987e5d13df3161
DmitryVakhrushev <dm.vakhrushev@gmail.com>
2019-05-03 12:49:06 -0400

    Hello World program

---
+++ C_CalebCurry/Caleb01_HelloWorld/Caleb01_HelloWorld/main.c
@@ -0,0 +1,13 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	
+	printf("Hello world!\n");
+		
+	//system("pause"); //this pauses the program until you press any key 
+
+	return 0;
+}
