936dff8e0e9eaa82c2a713e7de12e7e3b91757d2
DmitryVakhrushev <dm.vakhrushev@gmail.com>
2019-05-16 16:17:24 -0400

    Suppressed warnings before the #include statements

--- C_CalebCurry/Solution_C_Programming/Caleb14_Strings/main.c
+++ C_CalebCurry/Solution_C_Programming/Caleb14_Strings/main.c
@@ -1,25 +1,28 @@
+// In case of the error C4996: 'scanf': This function or variable may be unsafe.
+//To disable the compiler warning, add "before the #include lines"
+// https://stackoverflow.com/questions/30577519/error-c4996-scanf-this-function-or-variable-may-be-unsafe-in-c-programming
+#define _CRT_SECURE_NO_WARNINGS
+
 #include <stdio.h>
 #include <stdlib.h>
 
-//To disable the compiler warning, add to the very top of your program the following statement :
-#define _CRT_SECURE_NO_WARNINGS
-
 int main()
 {
-
 	// create a character array.
 	// if we need to store 30 characters create an array of 31
-	// as we need to store the "null terminator \0"
+	// as we need an extra character to store the "null terminator \0"
 
 	char name[31];
 	printf("Yo gurl enter yo name:\n");
 	
-	// we do not need to use & as it's for variables not arrays
-	//scanf("%s", name);
-	scanf_s("%s", name, (unsigned)_countof(name) ); // get an input from user and store in the array
+	// get an input from user and store in the array
+	// scanf is depricated. Use scanf_s instead
+	scanf("%s", name); // we do not need to use & as it's for variables not arrays
+
+	//scanf_s("%s", name, (unsigned)_countof(name) ); // scanf_s requires #include <stdlib.h>
 
 	printf("Hello %s\n", "Caleb!"); // a string is a sequence of characters
-	printf("Hello %s\nYou look very nice today.\n", name); // a string is a sequence of characters
+	printf("Hello %s! You look very nice today!!!\n", name); // a string is a sequence of characters
 
 	return 0;
 }
