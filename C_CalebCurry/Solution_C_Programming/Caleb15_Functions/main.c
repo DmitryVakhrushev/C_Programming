df75b313b57c9765cc4b9be797371bfe7b8405fb
DmitryVakhrushev <dm.vakhrushev@gmail.com>
2019-05-21 16:35:32 -0400

    Added Microcontrollers folder to keep AVR code and data sheets

---
+++ C_CalebCurry/Solution_C_Programming/Caleb15_Functions/main.c
@@ -0,0 +1,34 @@
+
+/*
+Author:
+Date:
+Description:
+*/
+
+#include <stdio.h> // add a library
+void greeting(); // prototype a function
+
+int main()
+{
+
+	greeting();
+
+	return 0;
+}
+
+void greeting() {
+
+	printf("Hello Kitty\n");
+
+	//TODO: Make the function use parameters
+	return; // every function needs "return" statement
+}
+
+// a single line comment
+
+/*
+A multi
+line
+comment
+*/
+
---
+++ Microcontrollers/Solution_AVR_Programs/ATmega8A_04_TurnOnLED/main.c
@@ -0,0 +1,18 @@
+/*
+ * ATmega8A_04_TurnOnLED.c
+ *
+ * Created: 05/21/2019 4:26:35 PM
+ * Author : dvakhru
+ */ 
+
+#include <avr/io.h> // adding a library (dvakhru)
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
