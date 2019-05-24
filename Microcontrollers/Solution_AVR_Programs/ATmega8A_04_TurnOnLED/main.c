8b246aaf2273b71a7fae8658acd0971f0497aa27
DmitryVakhrushev <dm.vakhrushev@gmail.com>
2019-05-24 15:25:36 -0400

    Set port direction (in or out). Set port voltage (+5V)

--- Microcontrollers/Solution_AVR_Programs/ATmega8A_04_TurnOnLED/main.c
+++ Microcontrollers/Solution_AVR_Programs/ATmega8A_04_TurnOnLED/main.c
@@ -1,6 +1,5 @@
 /*
  * ATmega8A_04_TurnOnLED.c
- *
  * Created: 05/21/2019 4:26:35 PM
  * Author : dvakhru
  */ 
@@ -10,9 +9,24 @@
 
 int main(void)
 {
-    /* Replace with your application code */
-    while (1) 
+    
+	/*
+	DDR - Direct Data Register (DDRB, DDRC, DDRC)
+	Direction: 0 - in, 1 - out
+	0b - binary representation, 0x - 16x representation
+	*/
+	
+	// Set port direction (in or out)
+	DDRC = 0b0000001; // set the PC0 = "out". (Port C has 7 bits)
+	DDRB = 0b10001001; // set the PB0, PB3, PB7 = "out". (Port B has 8 bits)
+	
+	// Set port voltage (+5V)
+	PORTC = 0b0000001; // set +5V for PCO
+	
+	while (1) // always true condition
     {
+		// your code here
+		
     }
 }
 
